55dde330ec966ce75cc8950579096161c54d8f75
bajran <youraksh@gmail.com>
2020-02-12 18:17:01 +0530

    Project on Custom video player

---
+++ Custom Video Player - In Vanilla Javascript/script.js
@@ -0,0 +1,5 @@
+const video = document.getElementById('video')
+const play = document.getElementById('play')
+const stop = document.getElementById('stop')
+const progress = document.getElementById('progress')
+const timestamp = document.getElementById('timestamp')
